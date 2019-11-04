1d791f73c232c6f9c21b7fc735b9af4b1597a9c2
Ryusakib <56534870+Ryusakib@users.noreply.github.com>
2019-11-04 11:40:35 +0100

    Update formulaire.js

---
+++ tp2/formulaire.js
@@ -0,0 +1,21 @@
+function validation() {
+	this.event.preventDefault(); 
+	var error = "";
+
+	if (document.getElementById("txtNom").value.length == null)
+		error += "La saisie du nom est obligatoire<br/>";
+	if (document.getElementById("txtPrenom").value.length == null)
+		error += "La saisie du nom est obligatoire";
+	if (document.getElementById("txtAdress").value.length == null)
+		error += "La saisie du nom est obligatoire";
+	if (document.getElementById("txtEmail").value.length == null)
+		error += "La saisie du nom est obligatoire";
+
+	if(error == ""){
+		// OK
+		document.getElementById("resultat").innerHTML = "tout est ok"
+	}else{
+		document.getElementById("error").innerHTML = error
+	}
+	return false;
+}
